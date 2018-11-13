f96791522b9a61d22c034af5845804eef8add0a2
Nam Won Hyung <nwh0526@gmail.com>
2018-11-13 17:47:14 +0900

    update 파일 변경일 기준 filter 함수 추가

--- src/com/kcube/support/patch/PatchController.java
+++ src/com/kcube/support/patch/PatchController.java
@@ -1,7 +1,16 @@
 package com.kcube.support.patch;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -82,7 +91,81 @@ public class PatchController {
 		destPathField.setText(path);
 	}
 
+	/**
+	 *
+	 * @param event
+	 * @throws Exception
+	 */
 	public void makePatchFile(ActionEvent event) throws Exception {
+		final String sourcePath = sourcePathField.getText();
+		final String destPath = destPathField.getText();
+		final LocalDate baseDate = baseDateField.getValue();
+
+		final boolean isPass = validateInput(sourcePath, destPath, baseDate);
+		if(isPass == false) return;
+
+		final List<Path> fileList = getCopyFileList(sourcePath, baseDate);
+		copy(destPath, fileList);
+	}
+
+	/**
+	 *
+	 * @param sourcePath
+	 * @param baseDate
+	 * @return
+	 * @throws Exception
+	 */
+	private List<Path> getCopyFileList(final String sourcePath, final LocalDate baseDate) throws Exception {
+		final Path path = Paths.get(sourcePath);
+		final boolean isExists = Files.exists(path);
+		if(isExists == false) {
+			AlertUtil.showAndWaitForError("패치 파일 생성 에러", "소스 파일 경로는 필수값입니다.");
+			throw new FileNotFoundException();
+		}
+
+		return Files.walk(path).filter(p -> {
+			long lastModifiedTime = 0L;
+			try {
+				lastModifiedTime = Files.getLastModifiedTime(p).toMillis();
+			} catch (IOException e) {
+			}
+			final Instant instant = Instant.ofEpochMilli(lastModifiedTime);
+			final LocalDate modifiedDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+
+			return modifiedDate.isAfter(baseDate);
+		}).collect(Collectors.toList());
+	}
+
+	/**
+	 *
+	 * @param destPath
+	 * @param fileList
+	 */
+	private void copy(final String destPath, final List<Path> fileList) {
+		fileList.stream().forEach(System.out::println);
+	}
+
+	/**
+	 *
+	 * @param sourcePath
+	 * @param destPath
+	 * @param baseDate
+	 * @return
+	 */
+	private boolean validateInput(final String sourcePath, final String destPath, final LocalDate baseDate) {
+		if(StringUtils.isEmpty(sourcePath)) {
+			AlertUtil.showAndWaitForError("패치 파일 생성 에러", "소스 파일 경로는 필수값입니다.");
+			return false;
+		}
+		if(StringUtils.isEmpty(destPath)) {
+			AlertUtil.showAndWaitForError("패치 파일 생성 에러", "패치 파일 생성 위치 경로는 필수값입니다.");
+			return false;
+		}
+		if(baseDate == null) {
+			AlertUtil.showAndWaitForError("패치 파일 생성 에러", "파일 생성 기준일은 필수값입니다.");
+			return false;
+		}
+		return true;
 	}
 
 	/**
@@ -124,6 +207,6 @@ public class PatchController {
 		directoryChooser.setInitialDirectory(new File("C:\\"));
 		File dir = directoryChooser.showDialog(null);
 
-		return dir.getAbsolutePath();
+		return StringUtils.isEmpty(dir.getAbsolutePath()) ? " " : dir.getAbsolutePath();
 	}
 }
